7c67a99dae9696b1c2d4f7b503861edd4fb7d9d7
Dantallope <dantebraccialini@gmail.com>
2026-01-09 11:06:56 -0700

    Returns name and age

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
 
 int main(){
-  auto result = (10 <=> 20) > 0;
-  std::cout << result << std::endl;
+/*   
+  std::cout << "Hello C++20" <<std::endl;
+
+  int age = 20;
+  std::cout << "Age : " << age << std::endl;
+
+  std::cerr << "Error message : Something is wrong" << std::endl;
+  std::clog << "Log message : something happened" << std::endl;
+ */
+  int age1;
+  std::string name;
+
+  std::cout << "Please type your name and age : " << std::endl;
+
+  std::cin >> name;
+  std::cin >> age1;
+
+  std::cout << "Your name is " << name << " and you are " << age1 << " years old." << std::endl;
+  return 0;
 }
