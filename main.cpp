0fbdb98104100c2d4e989dd0d0bcad0559039400
Dantallope <dantebraccialini@gmail.com>
2026-01-09 11:13:10 -0700

    Program accepts full name

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
-/*   
+/*
   std::cout << "Hello C++20" <<std::endl;
 
   int age = 20;
@@ -14,9 +14,9 @@ int main(){
   int age1;
   std::string name;
 
-  std::cout << "Please type your name and age : " << std::endl;
+  std::cout << "Please type your full name and age : " << std::endl;
 
-  std::cin >> name;
+  std::getline(std::cin,name);
   std::cin >> age1;
 
   std::cout << "Your name is " << name << " and you are " << age1 << " years old." << std::endl;
